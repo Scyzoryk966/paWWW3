7253ed6b3e30eb0302768fe86d63bdf557f2d1f4
Scyzoryk <damianszefler96@gmail.com>
2020-01-18 21:28:53 +0100

    Dodałem pare filtrów operujących na pixelach

--- js/canvas.js
+++ js/canvas.js
@@ -3,7 +3,10 @@ imageLoader.addEventListener('change', handleImage, false);
 var canvas1 = document.getElementById('imageCanvas');
 var ctx1 = canvas1.getContext('2d');
 var canvas2 = document.getElementById('imageDraw');
-var ctx2 = canvas2.getContext('2d');
+var src = ctx1.getImageData(0, 0, canvas1.width, canvas1.height);
+var copy = src.createImageData(canvas1.width, canvas1.height)
+copy.data.set(src.data);
+
 
 function handleImage(e){
     var reader = new FileReader();
@@ -18,18 +21,53 @@ function handleImage(e){
         };
         img.src = event.target.result;
         $('.uploadhide').hide(1000);
-        $('canvas').css('height', '300');
+        $('canvas').css('max-height', $(document).height()*0.8);
         $('.afterUpload').show(1000);
     };
     reader.readAsDataURL(e.target.files[0]);
 }
 
-function narysuj() {  // rysuje kółeczko, trzeba wymyślić jak to anulować...
+function grayScale() {
+    var imgData = ctx1.getImageData(0, 0, canvas1.width, canvas1.height);
+    var pixels  = imgData.data;
+    for (var i = 0, n = pixels.length; i < n; i += 4) {
+        var grayscale = pixels[i] * .3 + pixels[i+1] * .59 + pixels[i+2] * .11;
+        pixels[i  ] = grayscale;        // red
+        pixels[i+1] = grayscale;        // green
+        pixels[i+2] = grayscale;        // blue
+    }
+    ctx1.putImageData(imgData, 0, 0);
+}
+
+function brighten() {
+    var imgData = ctx1.getImageData(0, 0, canvas1.width, canvas1.height);
+    var pixels  = imgData.data;
+    for (var i = 0, n = pixels.length; i < n; i += 4) {
+        var grayscale = pixels[i] * .3 + pixels[i+1] * .59 + pixels[i+2] * .11;
+        pixels[i] += 10;        // red
+        pixels[i+1] += 10;        // green
+        pixels[i+2] += 10;        // blue
+    }
+    ctx1.putImageData(imgData, 0, 0);
+}
+
+function darken() {
+    var imgData = ctx1.getImageData(0, 0, canvas1.width, canvas1.height);
+    var pixels  = imgData.data;
+    for (var i = 0, n = pixels.length; i < n; i += 4) {
+        var grayscale = pixels[i] * .3 + pixels[i+1] * .59 + pixels[i+2] * .11;
+        pixels[i] -= 10;        // red
+        pixels[i+1] -= 10;        // green
+        pixels[i+2] -= 10;        // blue
+    }
+    ctx1.putImageData(imgData, 0, 0);
+}
+
+function text() {
     var c = document.getElementById("imageDraw");
     var ctx = c.getContext("2d");
-    ctx.beginPath();
-    ctx.arc(95, 50, 40, 0, 2 * Math.PI);
-    ctx.stroke();
+    ctx.font = "30px Arial";
+    ctx.fillText($('#imgTxt').val(), 10, 50);
 }
 
 function resetOrg() {
