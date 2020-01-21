10581e7a1347380346ee1e2e5f2c6ce8d6aacb9b
Scyzoryk <damianszefler96@gmail.com>
2020-01-21 22:52:54 +0100

    dodałem ramkę, tekst itp.

--- js/canvas.js
+++ js/canvas.js
@@ -15,6 +15,7 @@ function copyFun() {
 
 var copy = copyFun();
 var copy2 = null;
+var copy3 = null;
 function handleImage(e){
     var reader = new FileReader();
     reader.onload = function(event){
@@ -25,11 +26,7 @@ function handleImage(e){
             canvas2.width = img.width;
             canvas2.height = img.height;
             ctx1.drawImage(img,0,0);
-            src = ctx1.getImageData(0, 0, canvas1.width, canvas1.height); // "reset" filtrów po pikselach
-            copy = ctx1.createImageData(src.width, src.height);
-            for(var i = 0; i < copy.data.length; i++){
-                copy.data[i] = src.data[i];
-            }
+            copy = copyFun();
 
         };
         img.src = event.target.result;
@@ -53,6 +50,18 @@ function grayScale() {
     ctx1.putImageData(imgData, 0, 0);
 }
 
+function border(){
+    var c = document.getElementById("imageCanvas");
+    var ctx = c.getContext("2d");
+    var grd = ctx.createRadialGradient((canvas1.width)/2, (canvas1.height)/2, (canvas1.width)/7, (canvas1.width)/2, (canvas1.height)/2, (canvas1.height));
+    grd.addColorStop(0, "rgba(0,0,0,0)");
+    grd.addColorStop(1, "black");
+
+// Fill with gradient
+    ctx.fillStyle = grd;
+    ctx.fillRect(0, 0, canvas1.width, canvas1.height);
+}
+
 function processSepia() {
     var r = [0, 0, 0, 1, 1, 2, 3, 3, 3, 4, 4, 4, 5, 5, 5, 6, 6, 7, 7, 7, 7, 8, 8, 8, 9, 9, 9, 9, 10, 10, 10, 10, 11, 11, 12, 12, 12, 12, 13, 13, 13, 14, 14, 15, 15, 16, 16, 17, 17, 17, 18, 19, 19, 20, 21, 22, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 39, 40, 41, 42, 44, 45, 47, 48, 49, 52, 54, 55, 57, 59, 60, 62, 65, 67, 69, 70, 72, 74, 77, 79, 81, 83, 86, 88, 90, 92, 94, 97, 99, 101, 103, 107, 109, 111, 112, 116, 118, 120, 124, 126, 127, 129, 133, 135, 136, 140, 142, 143, 145, 149, 150, 152, 155, 157, 159, 162, 163, 165, 167, 170, 171, 173, 176, 177, 178, 180, 183, 184, 185, 188, 189, 190, 192, 194, 195, 196, 198, 200, 201, 202, 203, 204, 206, 207, 208, 209, 211, 212, 213, 214, 215, 216, 218, 219, 219, 220, 221, 222, 223, 224, 225, 226, 227, 227, 228, 229, 229, 230, 231, 232, 232, 233, 234, 234, 235, 236, 236, 237, 238, 238, 239, 239, 240, 241, 241, 242, 242, 243, 244, 244, 245, 245, 245, 246, 247, 247, 248, 248, 249, 249, 249, 250, 251, 251, 252, 252, 252, 253, 254, 254, 254, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255];
     var g = [0, 0, 1, 2, 2, 3, 5, 5, 6, 7, 8, 8, 10, 11, 11, 12, 13, 15, 15, 16, 17, 18, 18, 19, 21, 22, 22, 23, 24, 26, 26, 27, 28, 29, 31, 31, 32, 33, 34, 35, 35, 37, 38, 39, 40, 41, 43, 44, 44, 45, 46, 47, 48, 50, 51, 52, 53, 54, 56, 57, 58, 59, 60, 61, 63, 64, 65, 66, 67, 68, 69, 71, 72, 73, 74, 75, 76, 77, 79, 80, 81, 83, 84, 85, 86, 88, 89, 90, 92, 93, 94, 95, 96, 97, 100, 101, 102, 103, 105, 106, 107, 108, 109, 111, 113, 114, 115, 117, 118, 119, 120, 122, 123, 124, 126, 127, 128, 129, 131, 132, 133, 135, 136, 137, 138, 140, 141, 142, 144, 145, 146, 148, 149, 150, 151, 153, 154, 155, 157, 158, 159, 160, 162, 163, 164, 166, 167, 168, 169, 171, 172, 173, 174, 175, 176, 177, 178, 179, 181, 182, 183, 184, 186, 186, 187, 188, 189, 190, 192, 193, 194, 195, 195, 196, 197, 199, 200, 201, 202, 202, 203, 204, 205, 206, 207, 208, 208, 209, 210, 211, 212, 213, 214, 214, 215, 216, 217, 218, 219, 219, 220, 221, 222, 223, 223, 224, 225, 226, 226, 227, 228, 228, 229, 230, 231, 232, 232, 232, 233, 234, 235, 235, 236, 236, 237, 238, 238, 239, 239, 240, 240, 241, 242, 242, 242, 243, 244, 245, 245, 246, 246, 247, 247, 248, 249, 249, 249, 250, 251, 251, 252, 252, 252, 253, 254, 255];
@@ -73,6 +82,7 @@ function processSepia() {
     }
     ctx1.putImageData(imageData, 0, 0);
 }
+
 
 function brightes(value) {
     var imgData = ctx1.getImageData(0, 0, canvas1.width, canvas1.height);
@@ -96,6 +106,11 @@ function contrastImage(contrast){
         d[i+2] = d[i+2]*contrast + intercept;
     }
     ctx1.putImageData(imgData, 0, 0);
+}
+
+function Saturation(value){
+    ctx1.filter = "saturate("+value+")";
+    ctx1.drawImage(ctx1.canvas,0,0);
 }
 
 $(document).on('input change', '#customRange1', function() {
@@ -125,6 +140,12 @@ $(document).on('input change', '#customRange2', function() {
     copy3 = copyFun();
 });
 
+$(document).on('input change', '#customRange3', function() {
+    var value = ($(this).val());
+    resetSaturate();
+    Saturation(value);
+});
+
 function resetBgnd() {
     ctx1.putImageData(copy, 0, 0);
 }
@@ -140,11 +161,22 @@ function resetBright() {
     }
 }
 
+function resetSaturate(){
+    if (copy3 == null) {
+        resetBright();
+    }
+    else{
+        ctx1.putImageData(copy3, 0, 0);
+    }
+}
+
 function text() {
     var c = document.getElementById("imageDraw");
     var ctx = c.getContext("2d");
-    ctx.font = "30px Arial";
-    ctx.fillText($('#imgTxt').val(), 10, 50);
+    var rozmiar = document.getElementById("customRange4").value;
+    console.log(rozmiar);
+    ctx.font = "bold "+rozmiar+"px Arial";
+    ctx.fillText($('#imgTxt').val(), document.getElementById("txtX").value, document.getElementById("txtY").value);
 }
 
 function resetAdd() {
